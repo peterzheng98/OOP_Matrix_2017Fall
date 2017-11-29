0ae8eafe238a3cb804689ec620f30deef89b6efd
peterzheng98 <33046323+peterzheng98@users.noreply.github.com>
2017-11-29 16:22:43 +0800

    Init2(NoCmake)
    Init2(NoCmake)

---
+++ cmake-build-debug/CMakeFiles/OOP_Matrix_2017Fall.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP_Matrix_2017Fall.dir/main.cpp.o"
+  "OOP_Matrix_2017Fall.pdb"
+  "OOP_Matrix_2017Fall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP_Matrix_2017Fall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
